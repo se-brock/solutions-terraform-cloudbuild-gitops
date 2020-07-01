f844ae23e3fa72d10d9b99c030c62a8e92b45da1
Stephen Brock <stephen_e_brock@homedepot.com>
2020-07-01 15:01:09 -0500

    updated for project

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="edw-infra-sandbox19090000"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "edw-infra-sandbox19090000-tfstate"
     prefix = "env/prod"
   }
 }
